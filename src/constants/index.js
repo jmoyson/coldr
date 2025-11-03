b8094cfe95fbd4d639babc0cb6e86e7f7aa7620c
jmoyson <moyson.jeremy@gmail.com>
2025-11-03 11:59:06 +0100

    V0.0.5

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@
  */
 
 export const APP_NAME = 'coldr';
-export const APP_VERSION = '0.0.3';
+export const APP_VERSION = '0.0.5';
 export const APP_DESCRIPTION = 'Run Cold emails campaigns from your terminal.';
 export const GITHUB_REPO_URL = 'github.com/jmoyson/coldr';
 export const SHARE_HINT_TEXT =
